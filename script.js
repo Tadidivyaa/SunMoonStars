fb8a9f04915a6a482e8155cce9d6bc465cbe6353
Tadidivyaa <126139531+Tadidivyaa@users.noreply.github.com>
2023-07-19 19:51:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function sun(){
+    document.getElementById("sun").src="https://static.vecteezy.com/system/resources/thumbnails/003/370/979/small/beautiful-clear-blue-sky-with-white-clouds-and-sun-free-photo.jpg";
+}
+function moon(){
+    document.getElementById("sun").src="https://images.pexels.com/photos/3489072/pexels-photo-3489072.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500";
+}
+function stars(){
+    document.getElementById("sun").src="https://images.theconversation.com/files/413827/original/file-20210729-21-neazw1.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1200&h=675.0&fit=crop";
+    
+}
